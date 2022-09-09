7f535f35af4b6159a3c9fcd5469eb843b6042f69
Lizzay16 <erinsayo1357@gmail.com>
2022-09-08 18:49:32 -0700

    my fifth task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,12 +11,13 @@
  *     */
 
 int main(void)
-	
+
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
-   
+
+
